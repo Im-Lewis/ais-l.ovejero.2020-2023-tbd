cf23ae226c419da1ebc029c55eb8dc0346b1694e
Im-Lewis <118623973+Im-Lewis@users.noreply.github.com>
2023-06-05 18:00:54 +0200

    Update SeleniumTest.java

--- src/test/java/es/codeurjc/ais/e2e/selenium/SeleniumTest.java
+++ src/test/java/es/codeurjc/ais/e2e/selenium/SeleniumTest.java
@@ -9,7 +9,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions; 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
